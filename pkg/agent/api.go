915120d3972aff34737d19616c2b61b7c662cfcc
cjh <paul911014@naver.com>
2020-08-12 21:47:24 +0900

    test agent

--- pkg/agent/api.go
+++ pkg/agent/api.go
@@ -1,6 +1,24 @@
 package agent
 
 type Cluster struct{
-	Primary string `json:"primary"`
-	Secondary []string `json:"secondary"`
+	Primary Primary `json:"primary"`
+	Secondary []Secondary `json:"secondary"`
+}
+
+type Primary struct{
+	AgentKey       string `json:"agentKey"`
+	IP             string `json:"ip"`
+	Port           int    `json:"port"`
+	IsActive       bool   `json:"isActive"`
+	LastAccessTime int64  `json:"lastAccessTime"`
+}
+type Secondary struct{
+	IP             string `json:"ip"`
+	Port           int    `json:"port"`
+	IsActive       bool   `json:"isActive"`
+}
+
+type AliveCheck struct{
+	Time           int64  `json:"time"`
+	IsActive       bool   `json:"isActive"`
 }
