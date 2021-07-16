3dbfe3d14b26325121be1974d94f67dae779eae2
yoosaemsol <yoosaemsol@gmail.com>
2021-07-16 11:25:14 +0900

    Generate agent installation script

--- console/src/components/overview/AgentOverview.js
+++ console/src/components/overview/AgentOverview.js
@@ -15,15 +15,18 @@ import {
   TableRow
 } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Form, Input, Select } from 'antd';
 import styled from '@emotion/styled/macro';
 import Copy from 'react-copy-to-clipboard';
 import { message } from 'antd';
 import { CopyOutlined as CopyOutlinedIcon } from '@ant-design/icons';
 import { getAgentList } from '../store/actions/klevrActions';
+import { Link as RouterLink } from 'react-router-dom';
+import { platform } from 'chart.js';
+
+const { Option } = Select;
 
 const AgentList = () => {
-  const [data, setData] = useState(null);
   const dispatch = useDispatch();
   const currentZone = useSelector((store) => store.zoneReducer);
   const agentList = useSelector((store) => store.agentListReducer);
@@ -70,10 +73,20 @@ const AgentList = () => {
 };
 
 const AddAgent = () => {
-  const dispatch = useDispatch();
   const currentZone = useSelector((store) => store.zoneReducer);
   const [visible, setVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
+  const [form] = Form.useForm();
+  const [agentValues, setAgentValues] = useState({
+    apiKey: '',
+    managerUrl: API_SERVER,
+    platform: '',
+    zoneId: ''
+  });
+
+  const [key, setKey] = useState(undefined);
+  const [platform, setPlatform] = useState(undefined);
+  const [agentScript, setAgentScript] = useState(undefined);
 
   const Wrapper = styled.div`
     width: 100%;
@@ -98,35 +111,82 @@ const AddAgent = () => {
     }
   `;
 
+  const fetchKey = () => {
+    let completed = false;
+    async function get() {
+      try {
+        const result = await axios.get(
+          `${API_SERVER}/inner/groups/${currentZone}/apikey`
+        );
+        if (!completed) setKey(result.data);
+        setAgentValues({
+          ...agentValues,
+          apiKey: result.data,
+          zoneId: currentZone
+        });
+      } catch (err) {
+        setKey(undefined);
+      }
+    }
+    get();
+    return () => {
+      completed = true;
+    };
+  };
+
+  useEffect(() => {
+    fetchKey();
+  }, []);
+
+  useEffect(() => {
+    fetchKey();
+  }, [currentZone]);
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
   const showModal = () => {
     setVisible(true);
   };
 
-  const handleOk = () => {
-    setConfirmLoading(true);
-
-    console.log('agent install 완료 후 모달 닫기');
-
-    async function get() {
-      const result = await axios.get(
-        `${API_SERVER}/inner/groups/${currentZone}/agents`
-      );
-      dispatch(getAgentList(result.data));
-
-      if (result.status === 200) {
-        setVisible(false);
-        setConfirmLoading(false);
-      }
-    }
-    get();
-  };
-
   const handleCancel = () => {
-    console.log('click cancel');
+    setPlatform(undefined);
+    setAgentScript(undefined);
+    setAgentValues({
+      ...agentValues,
+      platform: undefined
+    });
+    onReset();
     setVisible(false);
   };
 
-  const content = `"curl -sL gg.gg/provbee | TAGPROV=0.5 TAGKLEVR=0.2.15-SNAPSHOT K3S_SET=Y K_API_KEY="45908457773441f38f32db09ed733eda" K_PLATFORM="baremetal" K_MANAGER_URL="https://dev.nexclipper.io:8080/klevr" K_ZONE_ID="900" K_CLUSTER_NAME="testasa" bash"`;
+  const onPlatformChange = (value) => {
+    setPlatform(value);
+
+    setAgentValues({
+      ...agentValues,
+      platform: value
+    });
+  };
+
+  const makeScript = async () => {
+    if (platform === undefined) {
+      return;
+    }
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    const response = await axios.post(
+      `${API_SERVER}/install/agents/bootstrap`,
+      agentValues,
+      { headers }
+    );
+
+    setAgentScript(response.data);
+  };
 
   return (
     <>
@@ -136,25 +196,76 @@ const AddAgent = () => {
       <Modal
         title="Install Agent"
         centered
-        okText="Done"
         visible={visible}
-        onOk={handleOk}
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
+        footer={null}
       >
-        <Wrapper>
-          <Content>{content}</Content>
-          <x.div position="absolute" top="5px" right="5px">
-            <Copy
-              text={content}
-              onCopy={() => {
-                message.success('Copied');
-              }}
+        <Form
+          form={form}
+          name="control-ref"
+          labelCol={{
+            span: 5
+          }}
+          wrapperCol={{
+            span: 17
+          }}
+        >
+          <Form.Item required label="API Key" name="apikey" disabled>
+            {key && <Input name="apikey" placeholder={key} disabled />}
+            {!key && (
+              <RouterLink to="/app/zones">
+                <Button type="link">Register API Key</Button>
+              </RouterLink>
+            )}
+          </Form.Item>
+          <Form.Item
+            name="platform"
+            label="Platform"
+            rules={[
+              {
+                required: true,
+                message: 'Please select a Platform'
+              }
+            ]}
+          >
+            <Select
+              placeholder="Select a platform"
+              onChange={onPlatformChange}
+              allowClear
             >
-              <CopyOutlined />
-            </Copy>
+              <Option value="linux">linux</Option>
+              <Option value="baremetal">baremetal</Option>
+              <Option value="kubernetes">kubernetes</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item required label="Manager" name="manager">
+            <Input name="manager" disabled placeholder={API_SERVER} />
+          </Form.Item>
+          <Form.Item required label="Zone Id" name="zondId">
+            <Input name="zoneId" disabled placeholder={currentZone} />
+          </Form.Item>
+          <x.div textAlign="center" pt="10px" pb="40px">
+            <Button onClick={makeScript} htmlType="submit">
+              Create agent setup script
+            </Button>
           </x.div>
-        </Wrapper>
+        </Form>
+        {agentScript && (
+          <Wrapper>
+            <Content>{agentScript}</Content>
+            <x.div position="absolute" top="5px" right="5px">
+              <Copy
+                text={agentScript}
+                onCopy={() => {
+                  message.success('Copied');
+                }}
+              >
+                <CopyOutlined />
+              </Copy>
+            </x.div>
+          </Wrapper>
+        )}
       </Modal>
     </>
   );
