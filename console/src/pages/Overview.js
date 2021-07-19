34e6c913e04a78bb4c9e7a06bc8388f358818f37
yoosaemsol <yoosaemsol@gmail.com>
2021-07-19 16:09:28 +0900

    Add Credential to Overview Page

--- console/src/pages/Overview.js
+++ console/src/pages/Overview.js
@@ -11,6 +11,7 @@ import { x } from '@xstyled/emotion';
 
 import TaskOverview from 'src/components/overview/TaskOverview';
 import AgentOverview from 'src/components/overview/AgentOverview';
+import CredentialOverview from 'src/components/overview/CredentialOverview';
 
 const Dashboard = () => {
   return (
@@ -25,20 +26,7 @@ const Dashboard = () => {
         <Container maxWidth={false}>
           <TaskOverview />
           <AgentOverview />
-
-          <Card sx={{ mt: 3 }}>
-            <x.div display="flex" alignItems="center">
-              <CardHeader title="Credential" />
-            </x.div>
-            <Divider />
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'flex-end',
-                p: 2
-              }}
-            ></Box>
-          </Card>
+          <CredentialOverview />
         </Container>
       </Box>
     </>
