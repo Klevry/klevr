fbb1098d42c4afbccd40ef15460ec147a422f57c
yoosaemsol <yoosaemsol@gmail.com>
2021-07-20 10:18:26 +0900

    Return error if current password mismatch

--- console/src/components/settings/SettingsPassword.js
+++ console/src/components/settings/SettingsPassword.js
@@ -9,15 +9,21 @@ import {
   Divider,
   TextField
 } from '@material-ui/core';
+
 import { API_SERVER } from 'src/config';
 
 const SettingsPassword = (props) => {
+  const [pwValid, setPwValid] = useState(false);
+  const [updated, setUpdated] = useState(false);
+
   const [values, setValues] = useState({
     current: '',
     new: ''
   });
 
   const handleChange = (event) => {
+    setPwValid(false);
+
     setValues({
       ...values,
       [event.target.name]: event.target.value
@@ -25,8 +31,9 @@ const SettingsPassword = (props) => {
   };
 
   const updateHandler = async () => {
+    //비밀번호 업데이트가 401 에러가 납니다. 해결하기!
+    console.log(values.current);
     console.log(values.new);
-    console.log(values.confirm);
 
     const headers = {
       'Content-Type': 'multipart/form-data'
@@ -37,13 +44,19 @@ const SettingsPassword = (props) => {
     form.append('pw', values.current);
     form.append('cpw', values.new);
 
-    const response = await axios.post(
-      `${API_SERVER}/console/changepassword`,
-      form,
-      { headers }
-    );
+    try {
+      const response = await axios.post(
+        `${API_SERVER}/console/changepassword`,
+        form,
+        { headers }
+      );
 
-    console.log(response);
+      if (response.status === 200) {
+        setUpdated(true);
+      }
+    } catch (err) {
+      setPwValid(true);
+    }
   };
 
   return (
@@ -68,6 +81,7 @@ const SettingsPassword = (props) => {
             type="password"
             value={values.password}
             variant="outlined"
+            error={pwValid}
           />
           <TextField
             fullWidth
